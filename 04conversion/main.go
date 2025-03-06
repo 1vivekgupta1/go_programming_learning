ddda974d03ac109643bf4835a6950983a49c4a82
Vivek <1773379618+1vivekgupta1@users.noreply.github.com>
2025-03-07 00:11:40 +0530

    data variable

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -5,25 +5,25 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to our pizza app")
-
 	fmt.Println("Please rate our pizza between 1 and 5")
 
 	reader := bufio.NewReader(os.Stdin)
 
-	input , _ := reader.ReadString('\n')
+	input, _ := reader.ReadString('\n')
 
-	fmt.Println("Thanks for rating , ", input)
+	fmt.Println("Thanks for rating, ", input)
 
-	numRating, err := strconv.ParseFloat(stings.TrimSpace(input),64)
-	
-	if err!= nil{
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
 		fmt.Println(err)
-	}
-	else {
-		fmt.println("Added q to your raing:",numRatingnumRating)
+
+	} else {
+		fmt.Println("Added 1 to your rating: ", numRating+1)
 	}
 }
